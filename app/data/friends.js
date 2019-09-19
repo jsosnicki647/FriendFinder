e1f8ae5a2e3441bbb5006c445e0c79f02937eeed
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-09-19 13:51:55 -0400

    added friends and updated styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,33 @@
 var friendsArray = [
   {
-    name: "Ahmed",
-    photo: "ahmed@example.com",
-    scores: ["1","2","3","4","5","1","2","3","4","5"]
+    name: "Joey",
+    photo: "https://imgix.bustle.com/uploads/image/2018/8/30/540599ec-b13a-428c-8075-6f15bda099d6-joey-tribbianni-best-pickup-lines.png?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70",
+    scores: ["1","5","5","1","1","2","5","1","3","5"]
+  },
+  {
+    name: "Chandler",
+    photo: "https://www.cheatsheet.com/wp-content/uploads/2018/09/Chandler-on-Friends.png",
+    scores: ["1","3","2","3","4","2","1","4","2","4"]
+  },
+  {
+    name: "Ross",
+    photo: "https://cdn.evoke.ie/wp-content/uploads/2018/04/04155521/Ross-Friends-696x503.jpg",
+    scores: ["3","2","1","5","5","4","1","5","1","3"]
+  },
+  {
+    name: "Monica",
+    photo: "https://amp.insider.com/images/5c8279ebeb3ce821ef1247a2-750-562.jpg",
+    scores: ["5","1","1","4","5","5","3","4","2","3"]
+  },
+  {
+    name: "Rachel",
+    photo: "https://i.pinimg.com/originals/a3/0d/8f/a30d8ff70d0584e6612c6ceba5da4290.jpg",
+    scores: ["2","2","4","2","1","2","3","2","4","4"]
+  },
+  {
+    name: "Phoebe",
+    photo: "https://i.ytimg.com/vi/V-ShguTU0sw/maxresdefault.jpg",
+    scores: ["2","3","5","1","1","1","5","1","5","4"]
   }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@ const friendsData = require("../data/friends")
 
 module.exports = (app) => {
     app.get("/api/friends", (req, res) => res.json(friendsData))
+    
     app.post("/api/friends", (req, res) => {
         friendsData.push(req.body)
     })
